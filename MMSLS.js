d3fbdb1bbb1bfb154a6092b5d1bce3d33c7dd57e
杯不离手 <97387815+bbls-ai@users.noreply.github.com>
2023-12-20 05:52:25 +0800

    更新 MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,7 +20,7 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=28194471&pid=66af3f381486ee94f6d672b388932371&tid=bce415a5fba8550c90353dc7c572fdb1",
+    "https://www.maimemo.com/share/page?uid=28194471&pid=9f790befa99b163a5a7734eaedde30b8&tid=1fd4a6ee398e19cc83a1c8febed21344",
 
 
   ]
